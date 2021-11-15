5ccf1208dbabde40447c8b02ce39212dcca9b659
VikeMK <62164219+VikeMK@users.noreply.github.com>
2021-11-15 22:44:30 +1000

    Initial Commit

---
+++ Vike.Blog/Program.cs
@@ -0,0 +1,11 @@
+﻿using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+await Bootstrapper.Factory
+    .CreateWeb(args)
+    .DeployToGitHubPages(
+        "VikeMK",
+        "vikemk.github.io",
+        Config.FromSetting<string>("GITHUB_TOKEN"))
+    .RunAsync();
