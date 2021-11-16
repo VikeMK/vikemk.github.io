7ffb761670a0aaeb7c141e3a92e7714024c3f689
VikeMK <62164219+VikeMK@users.noreply.github.com>
2021-11-16 22:39:52 +1000

    Add favicon

--- Vike.Blog/Program.cs
+++ Vike.Blog/Program.cs
@@ -16,6 +16,20 @@ public class AdditionalAssetsPipeline : Pipeline
     public AdditionalAssetsPipeline()
     {
         Isolated = true;
-        ProcessModules = new ModuleList { new CopyFiles("./CNAME", "CNAME") };
+        ProcessModules = new ModuleList
+        {
+            new CopyFiles("./CNAME", "CNAME"),
+            new CopyFiles("./assets/favicon/favicon.ico", "favicon.ico"),
+        };
+    }
+}
+
+public class FaviconPipeline : Pipeline
+{
+    public FaviconPipeline()
+    {
+        InputModules = new() { new ExecuteConfig(Config.FromContext(_ => new ReadFiles("assets/favicon/favicon.ico"))) };
+        ProcessModules = new() { new SetDestination("favicon.ico") };
+        OutputModules = new() { new WriteFiles() };
     }
 }
