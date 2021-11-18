b2b5509ca94df5784949dcb1f30db833132a954b
VikeMK <62164219+VikeMK@users.noreply.github.com>
2021-11-18 12:24:09 +1000

    Move favicon to input folder

--- Vike.Blog/Program.cs
+++ Vike.Blog/Program.cs
@@ -1,8 +1,6 @@
 ﻿using Statiq.App;
 using Statiq.Common;
-using Statiq.Core;
 using Statiq.Web;
-using Statiq.Web.Pipelines;
 
 await Bootstrapper.Factory
     .CreateWeb(args)
@@ -11,26 +9,3 @@ await Bootstrapper.Factory
         "vikemk.github.io",
         Config.FromSetting<string>("GITHUB_TOKEN"))
     .RunAsync();
-
-public class AdditionalAssetsPipeline : Pipeline
-{
-    public AdditionalAssetsPipeline()
-    {
-        Isolated = true;
-        ProcessModules = new ModuleList
-        {
-            new CopyFiles("./CNAME", "CNAME"),
-        };
-    }
-}
-
-public class FaviconPipeline : Pipeline
-{
-    public FaviconPipeline()
-    {
-        Dependencies.Add(nameof(Inputs));
-        InputModules = new() { new ReadFiles("assets/favicon/favicon.ico") };
-        ProcessModules = new() { new SetDestination("favicon.ico") };
-        OutputModules = new() { new WriteFiles() };
-    }
-}
